c202575e7c3b6111e4bbeb0028ff5ebbe1923df6
jdupshaw862 <jdupshaw862@gmail.com>
2026-08-19 16:57:57 -0400

    remove misplaced auth.controller.ts

--- src/auth/src/auth/auth.controller.ts
+++ src/auth/src/auth/auth.controller.ts
@@ -1,17 +1,1 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  @Post('login')
-  login(@Body() body: any) {
-    return this.authService.login(body.email, body.password);
-  }
-
-  @Post('refresh')
-  refresh(@Body() body: any) {
-    return this.authService.refresh(body.refreshToken);
-  }
-}
